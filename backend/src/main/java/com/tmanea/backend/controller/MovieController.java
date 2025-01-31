8490c7eda82ff8006d1faf15904e0864d9a0ee17
tudor-manea <tudor.manea03@gmail.com>
2025-01-31 16:15:07 +0000

    add midnight scheduling to active movie sync

--- backend/src/main/java/com/tmanea/backend/controller/MovieController.java
+++ backend/src/main/java/com/tmanea/backend/controller/MovieController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
