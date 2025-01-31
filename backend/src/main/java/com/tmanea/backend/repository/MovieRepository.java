846c4330a08d95423681474621a82314725d7a7c
tudor-manea <tudor.manea03@gmail.com>
2025-01-31 19:52:46 +0000

    add new columns to database and change id type for movies to uuid

--- backend/src/main/java/com/tmanea/backend/repository/MovieRepository.java
+++ backend/src/main/java/com/tmanea/backend/repository/MovieRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, String> {
     List<Movie> findByTitleContainingIgnoreCase(String title);
 //    List<Movie> findByReleaseDateStartsWith(String year);
 //    List<Movie> findByRatingGreaterThanEqual(Double rating);
